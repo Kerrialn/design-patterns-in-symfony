cfbc1d87435604ccfa451ee5514c31b243b73cdc
kez <kerrialbeckettnewham@gmail.com>
2024-05-17 10:31:04 +0200

    add builder pattern

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\VehicleBuilder\Builder\JaguarBuilder;
+use App\Service\VehicleBuilder\Builder\LandRoverBuilder;
+use App\Service\VehicleBuilder\Contract\VehicleBuilderInterface;
+use App\Service\VehicleBuilder\Director\VehicleDirector;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class VehicleController extends AbstractController
+{
+
+
+    public function __construct(
+        #[Autowire(service: LandRoverBuilder::class)]
+        private readonly VehicleBuilderInterface $landRoverBuilder,
+         #[Autowire(service: JaguarBuilder::class)]
+         private readonly VehicleBuilderInterface $jaguarBuilder
+    )
+    {
+    }
+
+    #[Route(path: '/vehicle', name: 'vehicle')]
+    public function builderDesignPattern() : Response {
+        $vehicleDirector = new VehicleDirector();
+        $landRover = $vehicleDirector->makeLandRover($this->landRoverBuilder);
+        $jaguar = $vehicleDirector->makeJaguarFtypeR75Coupe($this->jaguarBuilder);
+        dd($landRover, $jaguar);
+    }
+
+}
