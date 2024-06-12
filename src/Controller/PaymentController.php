0586510745a2e3fafa7ebf957bcfea26737fce54
kez <kerrialbeckettnewham@gmail.com>
2024-06-12 10:07:23 +0200

    add facade pattern

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Enum\PaymentTypeEnum;
+use App\Service\Mailer\Facade\MailerFacade;
 use App\Service\PaymentProccessorFactory\PaymentFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +12,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class PaymentController extends AbstractController
 {
     public function __construct(
-        private readonly PaymentFactory $paymentFactory
+        private readonly PaymentFactory $paymentFactory,
+        private readonly MailerFacade $mailerFacade,
     ) {
     }
 
@@ -22,4 +24,15 @@ class PaymentController extends AbstractController
         $payment = $paymentProcessor->process(50.00);
         dd($payment);
     }
+
+    #[Route(path: '/send/email', name: 'payment_send_email')]
+    public function facadeDesignPattern(): Response
+    {
+        $this->mailerFacade->sendWelcomeEmail(recipient: 'bob@gm.com', name: 'Bob');
+
+        $paymentProcessor = $this->paymentFactory->createProcessor(PaymentTypeEnum::PAYPAL_PAYMENT);
+        $payment = $paymentProcessor->process(50.00);
+        dd($payment);
+    }
+
 }
