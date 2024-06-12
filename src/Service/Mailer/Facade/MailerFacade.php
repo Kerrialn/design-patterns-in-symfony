b0d0cce68963d17e1f3e199e9a553b85b0c7ab8e
kez <kerrialbeckettnewham@gmail.com>
2024-06-12 10:08:30 +0200

    fix cs

--- src/Service/Mailer/Facade/MailerFacade.php
+++ src/Service/Mailer/Facade/MailerFacade.php
@@ -8,26 +8,24 @@ use Symfony\Component\Mailer\MailerInterface;
 
 class MailerFacade
 {
-
     public const APPLICATION_NO_REPLY_EMAIL_ADDRESS = 'no-reply@example.com';
 
     public function __construct(
         private readonly MailerInterface $mailer
-    )
-    {
+    ) {
     }
 
     /**
      * @throws TransportExceptionInterface
+     * @param string[] $context
      */
     public function sendTemplatedEmail(
         string $from,
         string $to,
         string $subject,
         string $template,
-        array  $context = [],
-    ): void
-    {
+        array $context = [],
+    ): void {
         $email = (new TemplatedEmail())
             ->from($from)
             ->to($to)
@@ -38,6 +36,21 @@ class MailerFacade
         $this->send($email);
     }
 
+    public function sendWelcomeEmail(
+        string $recipient,
+        string $name
+    ): void {
+        $this->sendTemplatedEmail(
+            from: self::APPLICATION_NO_REPLY_EMAIL_ADDRESS,
+            to: $recipient,
+            subject: 'welcome!!!!',
+            template: 'emails/welcome.html.twig',
+            context: [
+                'name' => $name,
+            ]
+        );
+    }
+
     /**
      * @throws TransportExceptionInterface
      */
@@ -46,25 +59,7 @@ class MailerFacade
         try {
             $this->mailer->send($email);
         } catch (TransportExceptionInterface $exception) {
-            throw new $exception;
+            throw new $exception();
         }
     }
-
-    public function sendWelcomeEmail(
-        string $recipient,
-        string $name
-    ): void
-    {
-        $this->sendTemplatedEmail(
-            from: self::APPLICATION_NO_REPLY_EMAIL_ADDRESS,
-            to: $recipient,
-            subject: 'welcome!!!!',
-            template: 'emails/welcome.html.twig',
-            context: [
-                'name' => $name
-            ]
-        );
-    }
-
-
 }
